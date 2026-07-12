6b435e6f236d405a835ae36abb8be8a9be15f158
cristhiangamer23 <rocohanocristhian12@gmail.com>
2026-07-12 17:59:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 const pool = new Pool({
 
 connectionString: process.env.DATABASE_URL,
